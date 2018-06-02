f8c02d6d47119592cafa69d98ba69388838e5872
MilkForDunk <yubin2892@gmail.com>
2018-06-02 11:05:19 +0900

    NavBar 추가

--- test.js
+++ test.js
@@ -4,9 +4,4 @@ var topkek  = topList[0];
 console.log(topList);
 console.log(topkek);
 
-topkek.addEventListener("click", function()
-{
-    alert("AAAAAAAAAAAAAA");
-});
-
 document.write("Ach fuck");
