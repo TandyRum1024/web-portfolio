f53f1c3469b7e5b43fcd5faf7334caf5fb0635e1
MilkForDunk <yubin2892@gmail.com>
2018-06-02 10:06:01 +0900

    자바스크립트 추가

---
+++ test.js
@@ -0,0 +1,12 @@
+var topList = document.getElementsByClassName('top-dawg');
+var topkek  = topList[0];
+
+console.log(topList);
+console.log(topkek);
+
+topkek.addEventListener("click", function()
+{
+    alert("AAAAAAAAAAAAAA");
+});
+
+document.write("Ach fuck");
